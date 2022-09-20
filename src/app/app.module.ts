8c68b5160ae7742f12122c2137eb0502d564fc31
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-09-21 00:03:03 +0530

    import animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
@@ -12,7 +13,8 @@ import { GridCardsComponent } from './components/grid-cards/grid-cards.component
     GridCardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
