166e6011da6182780904a5d6f1e4940fc2479f9d
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-09-21 00:03:49 +0530

    card selection and game logic

--- src/app/components/grid-cards/grid-cards.component.ts
+++ src/app/components/grid-cards/grid-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 import { Card, State } from 'src/app/interfaces/card';
 
 @Component({
@@ -6,16 +6,32 @@ import { Card, State } from 'src/app/interfaces/card';
   templateUrl: './grid-cards.component.html',
   styleUrls: ['./grid-cards.component.css']
 })
-export class GridCardsComponent implements OnInit {
+export class GridCardsComponent implements OnInit, DoCheck {
 
   gridTemplate!: string;
   @Input() rows: number = 0;
   @Input() cols: number = 0;
   numberMap: Array<number> = Array();
   cards: Array<Card> = Array();
+  selectedCards: Array<Card> = Array();
   totalCards: number = 0;
 
   constructor() { }
+
+  ngDoCheck(): void {
+    if(this.selectedCards.length === 2) {
+      setTimeout(() => {
+        if(this.selectedCards[0].content === this.selectedCards[1].content)
+          this.selectedCards[0].state = this.selectedCards[1].state = State.REMOVED;
+        
+        else
+          this.selectedCards[0].state = this.selectedCards[1].state = State.DEFAULT;
+        
+        this.selectedCards = Array();
+      }, 2500);
+      
+    }
+  }
 
   ngOnInit(): void {
     this.totalCards = this.rows * this.cols;
@@ -46,6 +62,22 @@ export class GridCardsComponent implements OnInit {
       return num;
     }
     else return this.getUniqueRandomInt(min, max);
+  }
+
+  selectCard(index: number) {
+    if(this.selectedCards.length >= 2) return;
+
+    
+    if(this.cards[index].state === State.VISIBLE) {
+      this.cards[index].state = State.DEFAULT;
+      this.selectedCards.pop();
+    }
+    
+    this.cards[index].state = State.VISIBLE;
+    this.selectedCards.push(this.cards[index]);
+    
+
+    console.log(this.selectedCards);
   }
 
 }
