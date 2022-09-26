cf8057427e6a14b085b09ebaaf9de0060719822c
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-09-26 22:33:24 +0530

    added animations

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,38 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Card, State } from 'src/app/interfaces/card';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  animations: [
+    trigger('flip', [
+      state('Default', style({
+        transform: "none"
+      })),
+      state('Visible', style({
+        transform: "perspective(600px) rotateY(180deg)"
+      })),
+      state('Removed', style({
+        opacity: 0,
+        transform: "scale(0.1)"      
+      })),
+      transition("Default <-> Visible", [
+        animate("400ms")
+      ]),
+      transition("* -> Removed", [
+        animate("500ms")
+      ])
+    ])
+  ]
 })
+
 export class CardComponent implements OnInit {
   @Input() cardData!: Card;
   imgSrc!: String;
   imgAlt!: String;
+
   constructor() { }
 
   ngOnInit(): void {
